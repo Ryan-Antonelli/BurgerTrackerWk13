10d75c77d6ea37d621c94597aff4bb94f7e112d3
Ryan Antonelli <ryan.a.antonelli@gmail.com>
2020-06-11 16:48:13 -0700

    heroku deployment parameters added

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 
 const routes = require("./controllers/burgers_controllers.js");
 
+const { connect } = require("http2");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+      host: "	y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "	n2bc59z6qvccrxxz",
+      password: "ij6t57s7lxy6g3cd",
+      database: "kk3b7ieu8kcsa3o0"  
+    })
+}
+
 var PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
