dafe56f1438abe9f3c85857ff61eb6049ff5f2ec
Ryan Antonelli <ryan.a.antonelli@gmail.com>
2020-06-11 16:12:13 -0700

    added server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express"); 
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
+const app = express();
 
-var app = express();
+const routes = require("./controllers/burgersController.js");
 
+app.use(express.static("public"));
+
+// middleware apps
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on port:", PORT);
+  });
